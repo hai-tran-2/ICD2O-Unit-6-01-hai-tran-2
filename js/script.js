37a13525d6fba13f715beeab86c58c301809df85
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-05-09 02:10:46 +0000

    add webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * this function find type of triangle
+ */
+
+function calculteSumOfTwoNumber() {
+  // input
+  let firstNumber = parseFloat(document.getElementById("first-number").value)
+  let secondNumber = parseFloat(document.getElementById("second-number").value)
+  let sumOfNumbers = 0
+  let counter = 0
+
+  // process
+  while (counter < secondNumber) {
+    sumOfNumbers = sumOfNumbers + firstNumber
+     counter++
+     // output
+    document.getElementById("result").innerHTML = "The sum of two number is " + sumOfNumbers
+  }
+}
